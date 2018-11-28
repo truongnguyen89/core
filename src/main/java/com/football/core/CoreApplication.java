ff1a25da334a9e1c675157a043ccdaf7298ed0df
Nguyen Quang Truong <truongnguyen89@gmail.com>
2018-11-29 00:07:55 +0700

    fix

--- src/main/java/com/football/core/CoreApplication.java
+++ src/main/java/com/football/core/CoreApplication.java
@@ -1,8 +1,20 @@
 package com.football.core;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableAsync
+@EnableJpaAuditing
+@EnableAutoConfiguration
+@EnableFeignClients(basePackages = {"com.football.common.feign"})
+@ComponentScan(basePackages = "com.football.*")
+@EnableEurekaClient
 @SpringBootApplication
 public class CoreApplication {
 
